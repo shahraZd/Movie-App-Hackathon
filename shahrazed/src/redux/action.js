d3a8a2157437c347023e68faa388baca8989fd79
Sheherazad <sheherazad.08@gmail.com>
2019-08-05 10:56:44 +0100

    remove

--- shahrazed/src/redux/action.js
+++
@@ -1,9 +0,0 @@
-import {ADD, SUPP} from '../const/actionTypes'
-
-export const add=(payload)=>{
-    return{type:ADD, payload}
-}
-
-export const supp=(payload)=>{
-    return{type:SUPP, payload}
-}
